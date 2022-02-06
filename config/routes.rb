eff1da40149a1a2cac7f6e4ed4f740c1d722c0d0
LayV <myakinb@mail.ru>
2022-02-07 02:19:41 +0300

    Added AnswersContoller actions: index new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions do
+    resources :answers, only: %i[index new create]
+  end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :answer do
+    question
+
     body { "MyText" }
     correct { false }
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end

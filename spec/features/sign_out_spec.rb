1f57a0dd34b62e381b42fe579f93223703a77690
LayV <myakinb@mail.ru>
2022-02-10 00:12:33 +0300

    Added sign out spec

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to end user session
+  As an inauthentificated user
+  I'd like to be able to sign out
+}do
+
+  scenario 'Registered user tries to sign out' do    
+    user = create(:user)
+    sign_in(user)
+
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end

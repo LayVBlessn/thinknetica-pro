0d6583696e7b93c517cf882152caf45dfacb7272
Bogdan <myakinb@mail.ru>
2022-02-28 17:56:05 +0300

    Added editing with attachments

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -28,6 +28,8 @@ feature 'User can edit his question', "
       within '.question' do
         fill_in 'Title', with: 'edited question title'
         fill_in 'Body', with: 'edited question body'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+
         click_on 'Save'
 
         expect(page).to_not have_content question.title
@@ -35,6 +37,8 @@ feature 'User can edit his question', "
         expect(page).to have_content 'edited question title'
         expect(page).to have_content 'edited question body'
         expect(page).to_not have_selector 'textarea'
+        expect(page).to have_link 'spec_helper.rb'
+        expect(page).to have_link 'rails_helper.rb'
       end
     end
 

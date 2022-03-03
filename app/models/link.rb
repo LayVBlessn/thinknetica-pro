28a6699d9fa290e12a040d7550232e37e1033ce0
Bogdan <myakinb@mail.ru>
2022-03-03 15:13:29 +0300

    Added Gist rendering

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,12 @@
 class Link < ApplicationRecord
+  GIST_URL = /gist.github.com/
+
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
   validates :url, format: URI::DEFAULT_PARSER.regexp[:ABS_URI]
+
+  def is_gist?
+    url.match(GIST_URL)
+  end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+
+  describe 'Model methods' do
+    let!(:answer) { create(:answer) }
+    let(:link) { create(:link, linkable: answer, url: 'https://gist.github.com/LayVBlessn/681d0316da444f358ea4ba96f34323da')}
+    let(:link_two) { create(:link, linkable: answer, url: 'https://vk.com') }
+
+    it 'should check is link a gist or not' do
+      expect(link).to be_is_gist
+      expect(link_two).to_not be_is_gist
+    end
+  end
 end
